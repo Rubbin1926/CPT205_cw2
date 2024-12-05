c648c257f935077b5a85506cd3dfa411a90de7ad
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2024-12-05 14:55:52 +0800

    参数规范了些

--- CW2.cpp
+++ CW2.cpp
@@ -6,16 +6,23 @@
 #include <iostream>
 using namespace std;
 
-float carCenterX = 0.0f;
-float carCenterY = 0.0f;
-float carCenterZ = 0.0f;
-float carXspeed = 0.0f;
-float carYspeed = 0.0f;
-float carZspeed = 0.0f;
-bool carTurnLeft = false;
-bool carTurnRight = false;
+float planeSizeX = 1.0f; 
+float planeSizeY = 0.2f;
+float planeSizeZ = 1.0f;
+float planeCenterX = 0.0f;
+float planeCenterY = 0.0f;
+float planeCenterZ = 0.0f;
+float planeXspeed = 0.5f;
+float planeYspeed = 0.0f;
+float planeZspeed = 0.0f;
+bool planeTurnLeft = false;
+bool planeTurnRight = false;
 bool crashed = false;
 float startHeight = 10.0;
+
+float groundX = 500.0;
+float groundY = 0.5;
+float groundZ = 500.0;
 
 int frameRate = 60; // Desired frame rate (frames per second)
 
@@ -34,27 +41,12 @@ float fltViewingAngle = 1;
 int fltMode = 0;
 
 
-void setupLightingAndMaterial(float red, float green, float blue) {
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    // Set light source position
-    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };  // Position above the scene
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    // Set light source properties
-    GLfloat light_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+void setupMaterial(float red, float green, float blue) {
 
     // Set material properties for the object
     GLfloat material_ambient[] = { red, green, blue, 1.0 };
     GLfloat material_diffuse[] = { red, green, blue, 1.0 };
-    GLfloat material_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat material_specular[] = { 0.0, 0.0, 0.0, 1.0 };
     GLfloat material_shininess[] = { 50.0 };
 
     glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
@@ -63,8 +55,33 @@ void setupLightingAndMaterial(float red, float green, float blue) {
     glMaterialfv(GL_FRONT, GL_SHININESS, material_shininess);
 }
 
+void initLighting() {
+    glEnable(GL_LIGHTING);
+
+    GLfloat light_position0[] = { 100.0, 100.0, -100.0, 1.0 };
+    GLfloat diffuse_specular_light0[] = { 0.8, 0.8, 0.8, 1.0 };
+    GLfloat ambient_light0[] = { 1.0, 1.0, 1.0, 1.0 };
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_specular_light0);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, diffuse_specular_light0);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light0);
+
+    glEnable(GL_LIGHT0);
+
+
+    GLfloat light_position1[] = { 1.0, 1.0, -0.0, 1.0 };
+    GLfloat diffuse_specular_light1[] = { 0.0, 0.0, 0.0, 1.0 };
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position1);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_specular_light1);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, diffuse_specular_light1);
+
+    glEnable(GL_LIGHT1);
+}
+
 void drawCube(float width, float height, float depth, float centerX, float centerY, float centerZ, float xAngle, float yAngle, float zAngle, float red, float green, float blue) {
-    setupLightingAndMaterial(red, green, blue);
+    setupMaterial(red, green, blue);
 
     float halfWidth = width / 2.0;
     float halfHeight = height / 2.0;
@@ -170,61 +187,56 @@ void drawEZCube(float width, float height, float depth, float centerX, float cen
     drawCube(width, height, depth, centerX, centerY, centerZ, 0.0, 0.0, 0.0, red, green, blue);
 }
 
-void drawCar(float size, float centerX, float centerY, float centerZ, float red, float green, float blue) {
-    setupLightingAndMaterial(red, green, blue);
+void drawPlane(float size, float centerX, float centerY, float centerZ, float red, float green, float blue) {
+    setupMaterial(red, green, blue);
 
     float wheel_size = 1.0 * size;
-    float distance = carCenterX;
+    float distance = planeCenterX;
 
     glEnable(GL_LIGHTING);
 
-    // Car body
-    drawEZCube(1.0 * size, 0.2 * size, 0.6 * size, centerX, centerY, centerZ, red, green, blue);
+    // Plane body
+    drawEZCube(planeSizeX * size, planeSizeY * size, planeSizeZ * size, centerX, centerY, centerZ, red, green, blue);
 
     // Draw wheels in cube
     float rotateZ = 1.0;
     float rotateYangle = 0.0;
-    if (carTurnLeft == true && carTurnRight == false) {
+    if (planeTurnLeft == true && planeTurnRight == false) {
         rotateYangle = M_PI / 4;
         rotateZ = 0.0;
     };
-    if (carTurnLeft == false && carTurnRight == true) {
+    if (planeTurnLeft == false && planeTurnRight == true) {
         rotateYangle = -M_PI / 4;
         rotateZ = 0.0;
     };
-    if (carTurnLeft == false && carTurnRight == false) {
+    if (planeTurnLeft == false && planeTurnRight == false) {
         rotateYangle = 0.0;
         rotateZ = 1.0;
     };
-    drawCube(0.15 * size, 0.15 * size, 0.05 * size, -0.3 * size + centerX, -0.15 * size + centerY, -0.3 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
-    drawCube(0.15 * size, 0.15 * size, 0.05 * size, -0.3 * size + centerX, -0.15 * size + centerY, +0.3 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
-    drawCube(0.15 * size, 0.15 * size, 0.05 * size, +0.3 * size + centerX, -0.15 * size + centerY, -0.3 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
-    drawCube(0.15 * size, 0.15 * size, 0.05 * size, +0.3 * size + centerX, -0.15 * size + centerY, +0.3 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
-
-    glDisable(GL_LIGHTING);
+    drawCube(0.15 * size, 0.15 * size, 0.05 * size, -planeSizeX * 0.3 * size + centerX, -planeSizeY * 0.5 * size + centerY, -planeSizeZ * 0.5 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
+    drawCube(0.15 * size, 0.15 * size, 0.05 * size, -planeSizeX * 0.3 * size + centerX, -planeSizeY * 0.5 * size + centerY, +planeSizeZ * 0.5 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
+    drawCube(0.15 * size, 0.15 * size, 0.05 * size, +planeSizeX * 0.3 * size + centerX, -planeSizeY * 0.5 * size + centerY, -planeSizeZ * 0.5 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
+    drawCube(0.15 * size, 0.15 * size, 0.05 * size, +planeSizeX * 0.3 * size + centerX, -planeSizeY * 0.5 * size + centerY, +planeSizeZ * 0.5 * size + centerZ, 0.0, rotateYangle, (-distance / 0.15) * rotateZ, 0.2, 0.2, 0.2);
 
     glEnd();
 }
 
 void drawGround(float centerX, float centerY, float centerZ) {
-    float groundWidth = 500.0;
-    float groundHeight = 0.5;
-    float groundDepth = 500.0;
-    float r = 0.15;
-    float g = 0.1;
-    float b = 0.15;
-    drawCube(groundWidth, groundHeight, groundDepth, centerX, centerY, centerZ, 0.0, 0.0, 0.0, r, g, b);
+    float r = 0.10;
+    float g = 0.10;
+    float b = 0.10;
+    drawCube(groundX, groundY, groundZ, centerX, centerY, centerZ, 0.0, 0.0, 0.0, r, g, b);
 }
 
 void drawDashedLine(float centerX, float centerY, float centerZ) {
-    float LineWidth = 0.6;
-    float LineHeight = 0.1;
-    float LineDepth = 0.05;
+    float LineX = 0.6;
+    float LineY = 0.1;
+    float LineZ = 0.05;
     float r = 0.9;
     float g = 0.9;
     float b = 0.9;
-    for (float X = -250.0; X < 250.0; X += 1.5) {
-        drawCube(LineWidth, LineHeight, LineDepth, X + centerX, centerY - LineHeight / 2, centerZ, 0.0, 0.0, 0.0, r, g, b);
+    for (float X = 0.0f; X < groundX / 2.0f; X += 1.5) {
+        drawCube(LineX, LineY, LineZ, X + centerX, centerY - LineY / 2, centerZ, 0.0, 0.0, 0.0, r, g, b);
     }
 }
 
@@ -241,24 +253,6 @@ void drawScene(float centerX, float centerY, float centerZ) {
     drawEZCube(0.2, 0.2, 0.2, groundX, groundY, groundZ, 0.0, 0.0, 1.0); // For background Test
 }
 
-void initLighting() {
-    // First point light source
-    GLfloat light_position0[] = { -1.0, 1.0, 0.0, 1.0 };
-    GLfloat light_diffuse0[] = { 0.0, 1.0, 0.0, 1.0 };
-    glEnable(GL_LIGHT0);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse0);
-
-    // Second point light source
-    GLfloat light_position1[] = { -1.0, 1.0, 0.0, 1.0 };
-    GLfloat light_diffuse1[] = { 0.0, 1.0, 0.0, 1.0 };
-    glEnable(GL_LIGHT1);
-    glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse1);
-
-    glEnable(GL_LIGHTING);
-}
-
 void handlefltMode() {
     switch (fltMode) {
     case 0:
@@ -273,26 +267,26 @@ void handlefltMode() {
 void handleKeypress(unsigned char key, int x, int y) {
     switch (key) {
     case 'e':
-        carXspeed += 0.02f;
-        carXspeed = min(carXspeed, 0.2f);
+        planeXspeed += 0.02f;
+        planeXspeed = min(planeXspeed, 0.2f);
         break;
     case 'd':
-        carXspeed -= 0.02f;
-        carXspeed = max(carXspeed, -0.2f);
+        planeXspeed -= 0.02f;
+        planeXspeed = max(planeXspeed, -0.2f);
         break;
     case 's':
-        carZspeed -= 0.02f;
-        carZspeed = min(carZspeed, 0.2f);
+        planeZspeed -= 0.002f;
+        planeZspeed = min(planeZspeed, 0.2f);
         break;
     case 'f':
-        carZspeed += 0.02f;
-        carZspeed = max(carZspeed, -0.2f);
+        planeZspeed += 0.002f;
+        planeZspeed = max(planeZspeed, -0.2f);
         break;
     case 'a':
-        carYspeed += 0.0005f;
+        planeYspeed += 0.001f;
         break;
     case 'z':
-        carYspeed -= 0.005f;
+        planeYspeed -= 0.01f;
         break;
     case ' ':
         handlefltMode();
@@ -304,51 +298,57 @@ void handleKeypress(unsigned char key, int x, int y) {
 
 void update(int value) {
     // Update the cube's position based on speed
-    if (carXspeed > 0) {
-        carXspeed -= 0.005f;
+    if (planeXspeed > 0) {
+        planeXspeed -= 0.02f * abs(planeXspeed);
     }
 
-    if (carXspeed < 0) {
-        carXspeed += 0.005f;
+    if (planeXspeed < 0) {
+        planeXspeed += 0.005f;
     }
 
-    if (carZspeed > 0) {
-        carZspeed -= 0.005f;
-        carTurnRight = true;
-        carTurnLeft = false;
+    if (planeZspeed > 0.0005f) {
+        planeZspeed -= 0.02f * abs(planeZspeed);
+        planeTurnRight = true;
+        planeTurnLeft = false;
     }
 
-    if (carZspeed < 0) {
-        carZspeed += 0.005f;
-        carTurnRight = false;
-        carTurnLeft = true;
+    if (planeZspeed < 0.0005f) {
+        planeZspeed += 0.01f * abs(planeZspeed);
+        planeTurnRight = false;
+        planeTurnLeft = true;
     }
 
-    if (carCenterY > -startHeight + 0.1) {
-        carYspeed -= 0.00005f;
+    if (planeCenterY > -startHeight + planeSizeY / 2.0f) {
+        planeYspeed -= 0.0001f;
     }
 
-    if (carCenterY < -startHeight + 0.1) {
-        carCenterY = -startHeight + 0.1;
-        carYspeed = - 0.1 * carYspeed;
+    if (planeCenterY < -startHeight + planeSizeY / 2.0f) {
+        planeCenterY = -startHeight + planeSizeY / 2.0f;
+        planeYspeed = -0.1 * planeYspeed;
     }
 
-    if (abs(carXspeed) < 0.005f) { carXspeed = 0; };
-    if (abs(carZspeed) < 0.005f) {
-        carZspeed = 0;
-        carTurnRight = false;
-        carTurnLeft = false;
+    if (abs(planeXspeed) < 0.005f) { 
+        planeXspeed = 0; 
     };
-    if (abs(carYspeed) < 0.1 * 0.00005f) { carYspeed = 0; };
 
-    carCenterX += carXspeed;
-    carCenterZ += carZspeed;
-    carCenterY += carYspeed;
+    if (abs(planeZspeed) < 0.0005f) {
+        planeZspeed = 0;
+        planeTurnRight = false;
+        planeTurnLeft = false;
+    };
+
+    if (abs(planeYspeed) < 0.1 * 0.0001f) { 
+        planeYspeed = 0; 
+    };
+
+    planeCenterX += planeXspeed;
+    planeCenterZ += planeZspeed;
+    planeCenterY += planeYspeed;
 
     std::cout << "__________" << endl;
-    std::cout << carXspeed << endl;
-    std::cout << carZspeed << endl;
-    std::cout << carYspeed << endl;
+    std::cout << planeXspeed << endl;
+    std::cout << planeZspeed << endl;
+    std::cout << planeYspeed << endl;
 
     switch (fltMode) {
     case 0:
@@ -389,8 +389,8 @@ void update(int value) {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    drawScene(0.0 - carCenterX, 0.0 - carCenterY, 0.0 - carCenterZ);
-    drawCar(1.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5);
+    drawScene(0.0 - planeCenterX, 0.0 - planeCenterY, 0.0 - planeCenterZ);
+    drawPlane(1.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5);
     drawEZCube(0.2, 0.2, 0.2, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
 
     glMatrixMode(GL_PROJECTION);
