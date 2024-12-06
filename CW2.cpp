757709c078aa43a6ca5ebc225bf16f2d5a050c28
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2024-12-07 01:05:39 +0800

    加了尾焰，云，开局的提示（位置要完善），需要做R键重启

--- CW2.cpp
+++ CW2.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <stdio.h> 
 #include <stdlib.h> 
-#include "vector" 
+#include <vector>
 #include <iostream>
 #define M_PI 3.14159265359
 
@@ -16,23 +16,26 @@ vector<GLubyte*>p;  // Similar to GLubyte* for program 3 but for 2 images (so a 
 GLuint texture[2];
 
 float planeWheelSize = 2.0;
-float planeSizeX = 20.0f; 
+float planeSizeX = 20.0f;
 float planeSizeY = 2.0f;
 float planeSizeZ = planeSizeY;
 float planeCenterX = 0.0f;
 float planeCenterY = 0.0f;
 float planeCenterZ = 0.0f;
-float planeXspeed = 30.0f;
+float planeXspeed = 20.0f;
 float planeYspeed = 0.0f;
 float planeZspeed = 0.0f;
+float lowXSpeed = 1.5f;
+float crashXSpeed = 0.5f;
 bool planeTurnLeft = false;
 bool planeTurnRight = false;
 bool crashed = false;
+bool crashReported = false;
 float startHeight = 88.0f;
 
-float groundX = 50000.0f;
-float groundY = 0.2f;
-float groundZ = 50000.0f;
+float groundSizeX = 50000.0f;
+float groundSizeY = 0.2f;
+float groundSizeZ = 50000.0f;
 
 int frameRate = 60; // Desired frame rate (frames per second)
 
@@ -48,7 +51,9 @@ float fltXUp; //Up vector
 float fltYUp;
 float fltZUp;
 float fltViewingAngle;
-int fltMode;
+
+int GameMode = 0;
+int frameCounter = 0;
 
 void ReadImage(const char path[256], GLint& imagewidth, GLint& imageheight, GLint& pixellength) {
     GLubyte* pixeldata;
@@ -259,7 +264,7 @@ void drawCube(float X_length, float Y_length, float Z_length, float centerX, flo
     glVertex3fv(vertices[4]);
 
     glEnd();
-    glDisable(GL_TEXTURE_2D);	
+    glDisable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
@@ -277,7 +282,7 @@ void drawPlane(float size, float centerX, float centerY, float centerZ, float re
 
     // Plane body
     //drawCube(planeSizeX * size, planeSizeY * size, planeSizeZ * size, centerX, centerY, centerZ, 0.0, 0.0, 0.0, red, green, blue, texture[0]);
-    drawCylinder(planeSizeY, 0, planeSizeX * 0.6, planeSizeX, 0.0, 0.0, 0.0, M_PI / 2, 0.0, 0.0, 0.0, 0.0, texture[0]);
+    drawCylinder(planeSizeY, 0, planeSizeX * 0.6, planeSizeX * 0.77, 0.0, 0.0, 0.0, M_PI / 2, 0.0, 0.0, 0.0, 0.0, texture[0]);
     drawCylinder(planeSizeY, planeSizeY, planeSizeX, 0.0, 0.0, 0.0, 0.0, M_PI / 2, 0.0, 0.0, 0.0, 0.0, texture[0]);
     drawCube(planeSizeX * 0.1 * size, planeSizeY * 0.2 * size, planeSizeX * 0.66 * size, centerX + planeSizeX * 0.5, centerY, centerZ - planeSizeZ * 1.7, 0.0, M_PI / 4, 0.0, red, green, blue, texture[0]);
     drawCube(planeSizeX * 0.1 * size, planeSizeY * 0.2 * size, planeSizeX * 0.66 * size, centerX + planeSizeX * 0.5, centerY, centerZ + planeSizeZ * 1.7, 0.0, -M_PI / 4, 0.0, red, green, blue, texture[0]);
@@ -294,7 +299,7 @@ void drawPlane(float size, float centerX, float centerY, float centerZ, float re
     float rotateZ = 0.0;
     float rotateZ_first = 0.0;
     float rotateYangle = 0.0;
-    bool onGroundEZ = (planeCenterY <= -startHeight + groundY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 1.5f);
+    bool onGroundEZ = (planeCenterY <= -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 1.5f);
     if (planeTurnLeft == true && planeTurnRight == false && onGroundEZ) {
         rotateYangle = M_PI / 6;
         rotateZ = 0.0;
@@ -321,6 +326,17 @@ void drawPlane(float size, float centerX, float centerY, float centerZ, float re
 
     drawCube(planeWheelSize * size, planeWheelSize * size, 0.5 * size, planeSizeX * 0.99 * size + centerX, -(planeSizeY) * 0.4 * size - planeWheelSize + centerY, 0.0, 0.0, 0.0, (-distance / planeWheelSize) * rotateZ_first, 0.5, 0.5, 0.5, 0);
 
+
+    // Fire
+    if (planeXspeed > 0.114514f) {
+        float fireSize = float(planeSizeY);
+        float disappearDistance = planeSizeX * planeXspeed / 30.0f;
+        for (float i = 1.0f; i >= 0; i -= 0.05f) {
+            drawEZCube(fireSize * i, fireSize * i, fireSize * i, disappearDistance * (i - 1.0f), 0.0, 0.0, 1.0, 0.7 * i, 0.0, 0);
+        }
+    }
+    
+
     glEnd();
 }
 
@@ -328,7 +344,7 @@ void drawGround(float centerX, float centerY, float centerZ) {
     float r = 0.10;
     float g = 0.10;
     float b = 0.10;
-    drawCube(groundX, groundY, groundZ, centerX, centerY, centerZ, 0.0, 0.0, 0.0, r, g, b, -1);
+    drawCube(groundSizeX, groundSizeY, groundSizeZ, centerX, centerY, centerZ, 0.0, 0.0, 0.0, r, g, b, -1);
 }
 
 void drawDashedLine(float centerX, float centerY, float centerZ) {
@@ -338,8 +354,24 @@ void drawDashedLine(float centerX, float centerY, float centerZ) {
     float r = 0.9;
     float g = 0.9;
     float b = 0.9;
-    for (float X = 0.0f; X < groundX / 2.0f; X += 15.0f) {
+    for (float X = 1660.0f; X < groundSizeX / 2.0f; X += 15.0f) {
         drawCube(LineX, LineY, LineZ, X + centerX, centerY, centerZ, 0.0, 0.0, 0.0, r, g, b, 0);
+    }
+}
+
+void drawClouds(float centerX, float centerY, float centerZ) {
+    float cloudSizeX = 50.0f;
+    float cloudSizeY = 2.0f;
+    float cloudSizeZ = 50.0f;
+    float interval = 2 * cloudSizeX;
+    float r = 0.9;
+    float g = 0.9;
+    float b = 0.9;
+
+    for (float X = 0.0f; X < groundSizeX / 2.0f; X += interval) {
+        for (float Z = -planeSizeZ * 100.0f; Z < planeSizeZ * 100.0f; Z += interval) {
+            drawCube(cloudSizeX, cloudSizeY, cloudSizeZ, X + centerX, centerY, Z + centerZ, 0.0, 0.0, 0.0, r, g, b, 0);
+        }
     }
 }
 
@@ -351,18 +383,22 @@ void drawScene(float centerX, float centerY, float centerZ) {
     drawGround(groundX, groundY, groundZ);
     drawDashedLine(groundX, groundY, groundZ - planeSizeZ * 10.0f);
     drawDashedLine(groundX, groundY, groundZ + planeSizeZ * 10.0f);
+    drawClouds(groundX, groundY + startHeight * 0.6f, groundZ);
 
 
     drawEZCube(2.0, 2.0, 2.0, groundX, groundY, groundZ, 0.0, 0.0, 1.0, 0); // For background Test
 }
 
-void handlefltMode() {
-    switch (fltMode) {
+void handleGameMode() {
+    switch (GameMode) {
     case 0:
-        fltMode = 1;
+        GameMode = 1;
         break;
     case 1:
-        fltMode = 0;
+        GameMode = 2;
+        break;
+    case 2:
+        GameMode = 1;
         break;
     }
 }
@@ -371,12 +407,18 @@ void handleKeypress(unsigned char key, int x, int y) {
     switch (key) {
     case 'E':
     case 'e':
-        planeXspeed += 0.2f;
-        planeXspeed = min(planeXspeed, 30.0f);
+        if (planeCenterY > -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 2.0f) {
+            planeXspeed += 0.3f;
+            planeXspeed = min(planeXspeed, 30.0f);
+        }
+        else {
+            planeXspeed += 0.15f;
+            planeXspeed = min(planeXspeed, 10.0f);
+        }
         break;
     case 'D':
     case 'd':
-        planeXspeed -= 0.2f;
+        planeXspeed -= 0.15f;
         planeXspeed = max(planeXspeed, -10.0f);
         break;
     case 'S':
@@ -400,10 +442,10 @@ void handleKeypress(unsigned char key, int x, int y) {
         planeYspeed -= 0.1f;
         break;
     case ' ':
-        handlefltMode();
+        handleGameMode();
         break;
-    case 'R':
-    case 'r':
+    case 'Q':
+    case 'q':
         glutFullScreenToggle();
         break;
     }
@@ -411,7 +453,7 @@ void handleKeypress(unsigned char key, int x, int y) {
     glutPostRedisplay();
 }
 
-void update(int value) {
+void updatePlane() {
     // Update the cube's position based on speed
     if (planeXspeed > 0 && planeCenterY > -startHeight + planeSizeY * 2.0f) {
         planeXspeed -= 0.01f * abs(planeXspeed);
@@ -441,32 +483,45 @@ void update(int value) {
         planeTurnLeft = true;
     }
 
-    if (planeCenterY > -startHeight + groundY / 2.0f + planeWheelSize / 1.0f + planeSizeY / 1.0f) {
+    if (planeCenterY > -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY / 1.0f) {
         planeYspeed -= 0.001f;
     }
 
-    if (planeCenterY < -startHeight + groundY / 2.0f + planeWheelSize / 1.0f + planeSizeY / 1.0f) {
-        planeCenterY = -startHeight + groundY / 2.0f + planeWheelSize / 1.0f + planeSizeY / 1.0f;
+    if (planeCenterY < -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY / 1.0f) {
+        planeCenterY = -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY / 1.0f;
         if (abs(planeYspeed) >= 0.5f) {
             crashed = true;
         }
         planeYspeed = -0.1 * planeYspeed;
     }
 
-    if (planeXspeed <= 2.0f && planeXspeed > 0.0 && planeCenterY > -startHeight + groundY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 2.0f) {
-        std::cout << "Low Speed!!!" << endl;
+    if (planeXspeed <= lowXSpeed &&
+        planeXspeed > crashXSpeed &&
+        planeCenterY > -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 2.0f &&
+        frameCounter % (frameRate / 6) == 0) {
+        if (crashed == false && !crashReported) {
+            std::cout << "Low Speed! (Almost Crash!)" << endl;
+        }
+        else {
+            std::cout << "Low Speed! (You can continue playing but you've already CRASHED once)" << endl;
+        }
+
     }
 
-    if (planeXspeed <= 0.7f && planeCenterY > -startHeight + groundY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 2.0f) {
+    if (planeXspeed <= crashXSpeed && planeCenterY > -startHeight + groundSizeY / 2.0f + planeWheelSize / 1.0f + planeSizeY * 2.0f) {
         crashed = true;
     }
 
-    if (abs(planeYspeed) >= 0.4f) {
+    if (abs(planeYspeed) >= 0.7f) {
         crashed = true;
     }
 
-    if (abs(planeXspeed) < 0.05f) { 
-        planeXspeed = 0; 
+    if (abs(planeCenterX) >= groundSizeX / 2 || abs(planeCenterZ) >= groundSizeZ) {
+        crashed = true;
+    }
+
+    if (abs(planeXspeed) < 0.05f) {
+        planeXspeed = 0;
     };
 
     if (abs(planeZspeed) < 0.005f) {
@@ -475,54 +530,81 @@ void update(int value) {
         planeTurnLeft = false;
     };
 
-    if (abs(planeYspeed) < 0.1 * 0.001f) { 
-        planeYspeed = 0; 
+    if (abs(planeYspeed) < 0.1 * 0.001f) {
+        planeYspeed = 0;
     };
 
     planeCenterX += planeXspeed;
     planeCenterZ += planeZspeed;
     planeCenterY += planeYspeed;
 
-    if (crashed == true) {
+    if (crashed == true && !crashReported) {
         std::cout << "Crashed!!!!!!!!!IDIOT" << endl;
+        crashReported = true;
     }
+
     //std::cout << "__________" << endl;
     //std::cout << planeXspeed << endl;
     //std::cout << planeZspeed << endl;
     //std::cout << planeYspeed << endl;
+    //std::cout << planeCenterX << endl;
+}
 
-    switch (fltMode) {
+void update(int value) {
+
+    if (GameMode != 0) {
+        updatePlane();
+    }
+
+    switch (GameMode) {
     case 0:
         fltFOV = 100.0; //Field Of View
         fltZoom = 1.0; //Zoom amount
         fltX0 = -20.0; //Camera position
-        fltY0 = 30.0;
+        fltY0 = 20.0;
         fltZ0 = -0.0;
         fltXRef = 0.0; //Look At reference point
         fltYRef = 0.0;
         fltZRef = 0.0;
-        fltXUp = 0.0; //Up vector
+        fltXUp = -0.5; //Up vector
         fltYUp = 1.0;
-        fltZUp = 0.0;
+        fltZUp = -0.0;
         fltViewingAngle = 1;
         break;
     case 1:
         fltFOV = 100.0; //Field Of View
         fltZoom = 1.0; //Zoom amount
-        fltX0 = 0.5; //Camera position
-        fltY0 = 0.5;
+        fltX0 = -20.0; //Camera position
+        fltY0 = 20.0;
+        fltZ0 = -0.0;
+        fltXRef = 0.0; //Look At reference point
+        fltYRef = 0.0;
+        fltZRef = 0.0;
+        fltXUp = -0.5; //Up vector
+        fltYUp = 1.0;
+        fltZUp = -0.0;
+        fltViewingAngle = 1;
+        break;
+    case 2:
+        fltFOV = 100.0; //Field Of View
+        fltZoom = 1.0; //Zoom amount
+        fltX0 = planeSizeX * 0.8; //Camera position
+        fltY0 = planeSizeY * 1.5;
         fltZ0 = 0.0;
-        fltXRef = 500.0; //Look At reference point
+        fltXRef = groundSizeX; //Look At reference point
         fltYRef = 0.5;
         fltZRef = 0.0;
-        fltXUp = 0.0; //Up vector
+        fltXUp = -0.5; //Up vector
         fltYUp = 1.0;
-        fltZUp = 0.0;
+        fltZUp = -0.0;
         fltViewingAngle = 1;
         break;
     }
 
-
+    frameCounter += 1;
+    if (frameCounter > 8192) {
+        frameCounter = 0;
+    }
     glutPostRedisplay();
     glutTimerFunc(1000 / frameRate, update, 0); // Call update function after 1/frameRate seconds
 }
@@ -532,7 +614,6 @@ void display() {
 
     drawScene(0.0 - planeCenterX, 0.0 - planeCenterY, 0.0 - planeCenterZ);
     drawPlane(1.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5);
-    drawEZCube(2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -544,7 +625,26 @@ void display() {
     glutSwapBuffers();
 }
 
+void consoleInit() {
+    std::cout << "Please Read the following instruction:" << endl;
+    std::cout << "Your mission is to land a plane smoothly on the airport runway." << endl;
+    std::cout << "If not handled properly, the plane will crash." << endl;
+    std::cout << "(For your own good experience, you can continue to fly freely even if you crash.)" << endl;
+    std::cout << "___________________________________________" << endl;
+    std::cout << "Note: " << endl;
+    std::cout << "Use the ESDF keys to control the acceleration of the FRONT, BACK, LEFT, and RIGHT directions." << endl;
+    std::cout << "Not WASD!!!" << endl;
+    std::cout << "Use A and Z to control the plane UP and DOWN." << endl;
+    std::cout << "The Q key is used to toggle FULL SCREEN, and the R key is used to TRY AGAIN." << endl;
+    std::cout << "The SPACE key is used to START the game or SWITCH to another person's perspective" << endl;
+    std::cout << "Tip: You can actually land smoothly by just pressing the E and A keys intermittently. But it's always good to try." << endl;
+    std::cout << "___________________________________________" << endl;
+    std::cout << "Now you can press the SPACE key to start the game." << endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+}
+
 void init() {
+    consoleInit();
     glClearColor(173.0f / 256.0f, 216.0f / 256.0f, 235.0 / 256.0f, 1.0);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
@@ -594,3 +694,5 @@ int main(int argc, char** argv) {
 // 仪表盘，旋转长方形就行
 // 飞机，可考虑螺旋桨，不用画圆
 // X轴上可以考虑用engine实现，不用一直按着
+// 
+// R键盘重启游戏，开局提示可以多次提示
